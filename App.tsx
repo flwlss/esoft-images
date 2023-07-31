3b78c0eb51a45a0985ad88a73d948a0b1fca62f8
Dmitry <dimaandreev1337@gmail.com>
2023-07-31 23:26:51 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>1234</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
